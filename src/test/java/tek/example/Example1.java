8fc3b9e8f049d31348a27e009f7f8b609f2d8435
mydocuments <hamed.mr.unique@gmail.com>
2024-09-23 19:37:58 -0700

    adding new line of code to new branch practice

--- src/test/java/tek/example/Example1.java
+++ src/test/java/tek/example/Example1.java
@@ -6,7 +6,13 @@ public class Example1 {
         String city = "Mansssas";
         System.out.println(city);
 
-        String city = "Manassas Park";
-        
+        String Localcity = "Manassas Park";
+        System.out.println(Localcity);
+
+        String name = "Hamed";
+        System.out.println(name);
+
+
+        // if you want to not add any folder to git repo just add .foldername in .gitignore file.
     }
 }
