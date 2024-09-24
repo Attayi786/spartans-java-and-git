77724789a94a53e63868c5a0ade159dc4770d36b
Attayi786 <hamed.mr.unique@gmail.com>
2024-09-23 19:30:50 -0700

    Update Example1.java

--- src/test/java/tek/example/Example1.java
+++ src/test/java/tek/example/Example1.java
@@ -5,5 +5,8 @@ public class Example1 {
     public static void main(String[] args) {
         String city = "Mansssas";
         System.out.println(city);
+
+        String city = "Manassas Park";
+        
     }
 }
